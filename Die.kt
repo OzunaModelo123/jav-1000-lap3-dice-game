73ce575b1abe9e8fe480be6326f3fa6dffe0798e
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-06-15 11:55:59 -0400

    Created Methods for the die class that returns a value

--- Die.kt
+++ Die.kt
@@ -7,3 +7,27 @@ enum class DiceType {
 class Die(private val sides: Int = 6, private var type: DiceType = DiceType.D6) {
     private var currentSide: Int = 1
 }
+
+init {
+    roll()
+}
+
+fun roll() {
+    currentSide = Random.nextInt(1, sides + 1)
+}
+
+fun getCurrentSide(): Int {
+    return currentSide
+}
+
+fun getType(): DiceType {
+    return type
+}
+
+fun setType(newType: DiceType) {
+    type = newType
+}
+
+fun getSides(): Int {
+    return sides
+}
