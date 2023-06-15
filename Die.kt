119372f555ace88fc6d8a782c6a6c3f61709fc2b
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-06-15 12:20:46 -0400

    retyped the code didn't follow instructions well

--- Die.kt
+++ Die.kt
@@ -1,33 +1,45 @@
 import kotlin.random.Random
 
-enum class DiceType {
-    D4, D6, D8, D10, D12, D20
+enum class DieType(val typeName: String) {
+    D4("d4"),
+    D6("d6"),
+    D8("d8"),
+    D10("d10"),
+    D12("d12"),
+    D20("d20"),
+    D100("d100")
 }
 
-class Die(private val sides: Int = 6, private var type: DiceType = DiceType.D6) {
-    private var currentSide: Int = 1
-}
+class Die(private var type: DieType = DieType.D6, private var sides: Int = 6) {
+    private var currentSide: Int = roll()
 
-init {
-    roll()
-}
+    fun getType(): DieType {
+        return type
+    }
 
-fun roll() {
-    currentSide = Random.nextInt(1, sides + 1)
-}
+    fun setType(type: DieType) {
+        this.type = type
+    }
 
-fun getCurrentSide(): Int {
-    return currentSide
-}
+    fun getSides(): Int {
+        return sides
+    }
 
-fun getType(): DiceType {
-    return type
-}
+    fun setSides(sides: Int) {
+        this.sides = sides
+    }
 
-fun setType(newType: DiceType) {
-    type = newType
-}
+    fun getCurrentSide(): Int {
+        return currentSide
+    }
+    init {
+        roll()
+    }
+    constructor(sides: Int) : this(DieType.valueOf("D$sides"), sides)
 
-fun getSides(): Int {
-    return sides
+    constructor(type: DieType, sides: Int) : this(type, sides)
+
+    fun roll() {
+        currentSide = Random.nextInt(1, sides + 1)
+    }
 }
